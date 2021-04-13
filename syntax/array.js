dcf091ffed2986b60db1afa3f038439677d21233
Hong <fheldgktpdy@naver.com>
2021-04-13 18:07:33 +0900

    saengcode

---
+++ syntax/array.js
@@ -0,0 +1,4 @@
+//CRUD
+var arr = ['a','b','c','d'];
+arr[2] = 'se'
+console.log(arr.length);
---
+++ syntax/template.js
@@ -0,0 +1,6 @@
+var name = 'qavo';
+var letter = 'JavaScript (/ˈdʒɑːvəˌskrɪpt/[6]), ' + name +'\
+\ often abbreviated as JS, is a high-level, ' + name + 'dynamic, weakly typed, prototype-based, multi-paradigm, and interpreted programming language. Alongside HTML and CSS, JavaScript is one of the three core technologies of World Wide Web content production. It is used to make webpages interactive and provide online programs, including video games. The majority of websites employ it, and all modern web browsers support it without the need for plug-ins by means of a built-in JavaScript engine. Each of the many JavaScript engines represent a different implementation of JavaScript, all based on the ECMAScript specification, with some engines not supporting the spec fully, and with many engines supporting additional features beyond ECMA.';
+
+console.log(letter);
+
